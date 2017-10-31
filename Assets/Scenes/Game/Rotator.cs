aa56491a55e4d47cdc16e28cd51c37f01d893ad7
Darie Calin <calin.darie@cegeka.com>
2017-10-31 23:00:40 +0200

    black players so they start with no rotation. black middle and attack rod now have a rotator script

---
+++ Assets/Scenes/Game/Rotator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+	public Vector3 rotationCenter;
+	void Start() {
+		var rod = gameObject.GetComponentsInChildren<Transform> ()
+			.First (o => o.name == "rod");
+		var rodRenderer = rod.GetComponent<Renderer> ();
+		rotationCenter = rodRenderer.bounds.center;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.RotateAround(
+			rotationCenter, 
+			new Vector3(1, 0, 0), 
+			180 * Time.deltaTime);
+	}
+}
