23336161164cfb2984ce7f8a7587a7ed9292f34d
Darie Calin <calin.darie@cegeka.com>
2017-11-20 21:14:01 +0200

    fixing slide/translation mouse control. damper(grip as i'm calling it) should have been non-zero. + script to replace men with prefab

---
+++ Assets/ReplaceObjectWindow.cs
@@ -0,0 +1,59 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class ReplaceSel : EditorWindow
+{
+    GameObject myObject;
+    [MenuItem("Tools/ReplaceSelected %g")]
+    public static void ReplaceObjects()
+    {
+        GetWindow(typeof(ReplaceSel));
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("Use Object", EditorStyles.boldLabel);
+
+        myObject = EditorGUILayout.ObjectField(myObject, typeof(GameObject), true) as GameObject;
+
+        if (!GUILayout.Button("Replace Selected")) return;
+        if (myObject == null) return;
+
+        int undoIndex = Undo.GetCurrentGroup();
+        foreach (Transform t in Selection.transforms)
+        {
+            GameObject o;
+            o = PrefabUtility.GetPrefabParent(myObject) as GameObject;
+
+            if (PrefabUtility.GetPrefabType(myObject).ToString() == "PrefabInstance")
+            {
+                o = (GameObject)PrefabUtility.InstantiatePrefab(o);
+                PrefabUtility.SetPropertyModifications(o, PrefabUtility.GetPropertyModifications(myObject));
+            }
+            else if (PrefabUtility.GetPrefabType(myObject).ToString() == "Prefab")
+            {
+                o = (GameObject)PrefabUtility.InstantiatePrefab(myObject);
+            }
+            else
+            {
+                o = Instantiate(myObject);
+            }
+
+            Undo.RegisterCreatedObjectUndo(o, "created prefab");
+
+            Transform newT = o.transform;
+
+            if (t == null) continue;
+            newT.position = t.position;
+            newT.rotation = t.rotation;
+            newT.localScale = t.localScale;
+            newT.parent = t.parent;
+        }
+
+        foreach (GameObject go in Selection.gameObjects)
+        {
+            Undo.DestroyObjectImmediate(go);
+        }
+        Undo.CollapseUndoOperations(undoIndex);
+    }
+}
