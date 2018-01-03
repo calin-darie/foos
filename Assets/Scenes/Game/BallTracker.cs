558711b1247bdf75e8585826f5fd9a630fced4bc
calin-darie <calin_darie@yahoo.com>
2018-01-03 16:58:13 +0200

    init ball position when game starts

--- Assets/Scenes/Game/BallTracker.cs
+++ Assets/Scenes/Game/BallTracker.cs
@@ -7,7 +7,7 @@ public class BallTracker : MonoBehaviour
     Vector3 _center;
     bool _ballActive;
     Rigidbody _ballRigidbody;
-    Vector3 _nextReseBallPositiontOffset = 2.5f*Vector3.forward + 2* Vector3.right;
+    Vector3 _nextReseBallPositiontOffset = 2.5f*Vector3.forward + 0.5f * Vector3.right;
 
     private bool BallActive
     {
@@ -20,18 +20,24 @@ public class BallTracker : MonoBehaviour
 	    _ballCollider = Ball.GetComponent<SphereCollider>();
 	    _ballRigidbody = Ball.GetComponent<Rigidbody>();
 	    _center = GetComponent<Renderer>().bounds.center;
-	}
+	    InitBallPosition();
+    }
 
     void Update()
 	{
 	    if (Input.GetKeyDown(KeyCode.R))
 	    {
 	        StopBall();
-	        _nextReseBallPositiontOffset = -_nextReseBallPositiontOffset;
-            Ball.position = _center + _ballCollider.radius * Vector3.up + _nextReseBallPositiontOffset;
-            BallActive = true;
+	        InitBallPosition();
+	        BallActive = true;
 	    }
 	}
+
+    private void InitBallPosition()
+    {
+        _nextReseBallPositiontOffset = -_nextReseBallPositiontOffset;
+        Ball.position = _center + _ballCollider.radius * Vector3.up + _nextReseBallPositiontOffset;
+    }
 
     private void StopBall()
     {
